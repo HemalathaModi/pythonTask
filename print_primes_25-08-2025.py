6d8fd16bb360d34593e9d5c02f403242c713f401
HemalathaModi <modihemalatha24@gmail.com>
2025-08-31 21:56:05 +0530

    Update print_primes_25-08-2025.py

--- print_primes_25-08-2025.py
+++ print_primes_25-08-2025.py
@@ -4,10 +4,12 @@ def prime_check(num):
             return False
     return True
 
-def print_primes(maxi):
-    for i in range(2 , maxi + 1):
+def print_primes(mini , maxi):
+    for i in range(mini , maxi + 1):
         if prime_check(i):
             print(i , end = ',')
 
+mini = int(input('Enter a min number from where you want to start printing prime numbers : '))
 maxi = int(input('Enter a max range you want to print prime numbers upto : '))
-print_primes(maxi)
+print_primes(mini , maxi)
+
